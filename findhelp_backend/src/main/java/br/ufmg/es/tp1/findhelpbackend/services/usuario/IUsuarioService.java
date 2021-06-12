1bf27515648d0c0012d9609226d2b2ff429ae03c
Vitor <assuncao26vitor@gmail.com>
2021-06-12 11:41:32 -0300

    Implementação Service

--- findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/services/usuario/IUsuarioService.java
+++ findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/services/usuario/IUsuarioService.java
@@ -1,4 +1,7 @@
 package br.ufmg.es.tp1.findhelpbackend.services.usuario;
 
+import java.util.UUID;
+
 public interface IUsuarioService {
+    UUID salvarDadosUsuario(String nome, String nomeUsuario, String senha, boolean psicologo);
 }
--- findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/services/usuario/UsuarioService.java
+++ findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/services/usuario/UsuarioService.java
@@ -1,4 +1,28 @@
 package br.ufmg.es.tp1.findhelpbackend.services.usuario;
 
+import br.ufmg.es.tp1.findhelpbackend.exceptions.ParametroInvalidoException;
+import br.ufmg.es.tp1.findhelpbackend.models.Usuario;
+import br.ufmg.es.tp1.findhelpbackend.repositories.usuario.IUsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class UsuarioService implements IUsuarioService {
+    private final IUsuarioRepository usuarioRepository;
+
+    @Autowired
+    public UsuarioService(IUsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public UUID salvarDadosUsuario(String nome, String nomeUsuario, String senha, boolean psicologo) {
+        if(nome == null|| nomeUsuario == null || senha == null || nome.isEmpty() || nomeUsuario.isEmpty() || senha.isEmpty()) {
+            throw new ParametroInvalidoException();
+        }
+        Usuario novoUsuario = new Usuario(UUID.randomUUID(), nome, nomeUsuario, senha, psicologo);
+        usuarioRepository.save(novoUsuario);
+        return novoUsuario.getId();
+    }
 }
