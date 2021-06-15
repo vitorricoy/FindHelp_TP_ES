4e363ab150243a148a37aeede443835c5d7c6c81
Vitor <assuncao26vitor@gmail.com>
2021-06-15 13:31:49 -0300

    Controller Atualizado - Login

--- findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/controllers/usuario/UsuarioController.java
+++ findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/controllers/usuario/UsuarioController.java
@@ -1,7 +1,9 @@
 package br.ufmg.es.tp1.findhelpbackend.controllers.usuario;
 
+import br.ufmg.es.tp1.findhelpbackend.services.usuario.ILoginService;
 import br.ufmg.es.tp1.findhelpbackend.services.usuario.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +15,21 @@ import java.util.UUID;
 public class UsuarioController {
     private final IUsuarioService usuarioService;
 
+    private final ILoginService loginService;
+
     @Autowired
-    public UsuarioController(IUsuarioService usuarioService) {
+    public UsuarioController(IUsuarioService usuarioService, ILoginService loginService) {
         this.usuarioService = usuarioService;
+        this.loginService = loginService;
     }
 
     @PostMapping
     UUID salvarDadosUsuario(String nome, String nomeUsuario, String senha, boolean psicologo) {
         return usuarioService.salvarDadosUsuario(nome, nomeUsuario, senha, psicologo);
+    }
+
+    @GetMapping(value = "/login")
+    UUID logarUsuario(String nomeUsuario, String senha) {
+        return loginService.logarUsuario(nomeUsuario, senha);
     }
 }
