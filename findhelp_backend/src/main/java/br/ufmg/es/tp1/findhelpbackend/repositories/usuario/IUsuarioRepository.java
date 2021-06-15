348d4a5fbe59bbeb2af5fbe2635fc86127b362b9
Vitor <assuncao26vitor@gmail.com>
2021-06-15 13:34:16 -0300

    Repository Atualizado - Login

--- findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/repositories/usuario/IUsuarioRepository.java
+++ findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/repositories/usuario/IUsuarioRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.UUID;
 
 public interface IUsuarioRepository extends CrudRepository<Usuario, UUID> {
-
+    Usuario findByNomeUsuario(String nomeUsuario);
 }
