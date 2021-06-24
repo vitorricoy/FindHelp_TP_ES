63def49045ae8e7781543f7146a60ad9f3049135
Vitor <assuncao26vitor@gmail.com>
2021-06-24 13:18:20 -0300

    Exceção criada para login inválido

--- findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/exceptions/LoginInvalidoException.java
+++ findhelp_backend/src/main/java/br/ufmg/es/tp1/findhelpbackend/exceptions/LoginInvalidoException.java
@@ -3,6 +3,7 @@ package br.ufmg.es.tp1.findhelpbackend.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
-public class LoginInvalidoException extends RuntimeException{
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class LoginInvalidoException extends RuntimeException {
+
 }
